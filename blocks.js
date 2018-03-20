4eae5570cc86758104a49539c11925e13a0fb4d5
Keeghan <keeghan_m@live.com>
2018-03-20 13:34:26 +0100

    Working

---
+++ blocks.js
@@ -0,0 +1,298 @@
+let blocks = [
+	{
+		"time":20,
+		"type":"21-17-13-9-5 reps",
+		"rounds":1,
+		"rest":null,
+		"exercises":[
+			{
+				"name":"DB Snatch",
+				"reps":null
+			},
+			{
+				"name":"Hanging Knee Raises",
+				"reps":null
+			},
+			{
+				"name":"Burpees",
+				"reps":null
+			},
+			{
+				"name":"Squat Thrusters",
+				"reps":null
+			}
+		]
+	},
+	{
+		"time":10,
+		"type":"EMOM 10",
+		"rounds":1,
+		"rest":null,
+		"exercises":[
+			{
+				"name":"Squat Thrusters",
+				"reps":7
+			},
+			{
+				"name":"Burpees",
+				"reps":3
+			}
+		]
+	},
+	{
+		"time":10,
+		"type":"EMOM 10",
+		"rounds":1,
+		"rest":null,
+		"exercises":[
+			{
+				"name":"Hanging Knee Raises",
+				"reps":10
+			},
+			{
+				"name":"Burpee Box Jumps",
+				"reps":5
+			}
+		]
+	},
+	{
+		"time":10,
+		"type":"EMOM 10",
+		"rounds":1,
+		"rest":null,
+		"exercises":[
+			{
+				"name":"Sprint",
+				"reps":null
+			},
+			{
+				"name":"Front Squat",
+				"reps":5
+			},
+			{
+				"name":"Sprint",
+				"reps":null
+			}
+		]
+	},
+	{
+		"time":8,
+		"type":"Circuit 30s on / 10s rest",
+		"rounds":4,
+		"rest":60,
+		"exercises":[
+			{
+				"name":"Getups",
+				"reps":null
+			},
+			{
+				"name":"Burpees",
+				"reps":null
+			},
+			{
+				"name":"Squat Thrusters",
+				"reps":null
+			},
+			{
+				"name":"KB Swing",
+				"reps":null
+			},
+			{
+				"name":"Jumping Lunges",
+				"reps":null
+			},
+			{
+				"name":"Skipping",
+				"reps":null
+			}
+		]
+	},
+	{
+		"time":5,
+		"type":"30-20-10 Seconds",
+		"rounds":1,
+		"rest":10,
+		"exercises":[
+			{
+				"name":"Cross Crunches",
+				"reps":null
+			},
+			{
+				"name":"Flitter Kicks",
+				"reps":null
+			},
+			{
+				"name":"Plank Hold",
+				"reps":null
+			}
+		]
+	},
+	{
+		"time":10,
+		"type":"EMOM 10",
+		"rounds":1,
+		"rest":null,
+		"exercises":[
+			{
+				"name":"Burpee Over Bar",
+				"reps":6
+			},
+			{
+				"name":"Burpee Over Bar",
+				"reps":7
+			},
+			{
+				"name":"Burpee Over Bar",
+				"reps":8
+			},
+			{
+				"name":"Burpee Over Bar",
+				"reps":9
+			},
+			{
+				"name":"Burpee Over Bar",
+				"reps":10
+			},
+			{
+				"name":"Burpee Over Bar",
+				"reps":10
+			},
+			{
+				"name":"Burpee Over Bar",
+				"reps":9
+			},
+			{
+				"name":"Burpee Over Bar",
+				"reps":8
+			},
+			{
+				"name":"Burpee Over Bar",
+				"reps":7
+			},
+			{
+				"name":"Burpee Over Bar",
+				"reps":6
+			}
+		]
+	},
+	{
+		"time":10,
+		"type":"EMOM 10",
+		"rounds":1,
+		"rest":null,
+		"exercises":[
+			{
+				"name":"Front Squat",
+				"reps":2
+			},
+			{
+				"name":"Front Squat",
+				"reps":3
+			},
+			{
+				"name":"Front Squat",
+				"reps":4
+			},
+			{
+				"name":"Front Squat",
+				"reps":5
+			},
+			{
+				"name":"Front Squat",
+				"reps":6
+			},
+			{
+				"name":"Front Squat",
+				"reps":6
+			},
+			{
+				"name":"Front Squat",
+				"reps":5
+			},
+			{
+				"name":"Front Squat",
+				"reps":4
+			},
+			{
+				"name":"Front Squat",
+				"reps":3
+			},
+			{
+				"name":"Front Squat",
+				"reps":2
+			}
+		]
+	},
+	{
+		"time":12,
+		"type":"Circuit 30s on / 10s rest",
+		"rounds":2,
+		"rest":null,
+		"exercises":[
+			{
+				"name":"Hanging Knee Raises",
+				"reps":null
+			},
+			{
+				"name":"Squat Thrusters",
+				"reps":null
+			},
+			{
+				"name":"Box Jumps",
+				"reps":null
+			},
+			{
+				"name":"Bicep Curls",
+				"reps":null
+			},
+			{
+				"name":"Wall Balls",
+				"reps":null
+			},
+			{
+				"name":"Tricep Dips",
+				"reps":null
+			},
+			{
+				"name":"Pull Ups",
+				"reps":null
+			},
+			{
+				"name":"Plank Hold",
+				"reps":null
+			}
+		]
+	},
+	{
+		"time":20,
+		"type":"Circuit 60s on / 30s rest",
+		"rounds":2,
+		"rest":null,
+		"exercises":[
+			{
+				"name":"Hanging Knee Raises",
+				"reps":null
+			},
+			{
+				"name":"Wall Balls",
+				"reps":null
+			},
+			{
+				"name":"Burpee Box Jumps",
+				"reps":null
+			},
+			{
+				"name":"Overhead Press",
+				"reps":null
+			},
+			{
+				"name":"Russian Twist",
+				"reps":null
+			},
+			{
+				"name":"Pistol Squats",
+				"reps":null
+			}	
+		]
+	}
+];
---
+++ generator.js
@@ -0,0 +1,100 @@
+let time = 30
+let workout = []
+
+function pickBlock (x) {
+  let block = null
+  counter = 0
+  while (block == null && counter < blocks.length * 2) {
+    counter++
+    let random = Math.floor(Math.random() * blocks.length)
+    block = blocks[random]
+    if (block.time <= x && workout.includes(block) == false) {
+      return block
+    }
+    block = null
+  }
+  return block
+}
+
+function setTime (x) {
+  time = x
+}
+
+function genWorkout (x) {
+  workout = []
+  setTime(x)
+
+  setWarmup()
+
+  while (time > 0) {
+    let block = pickBlock(time)
+    if (block) {
+      workout.push(block)
+      setTime(time - block.time)
+    } else {
+      return workout
+    }
+  }
+  return workout
+}
+
+function setWarmup () {
+  let warmup = {'time': 5,
+    'type': 'Warmup 30s on / 5s off',
+    'rounds': 1,
+    'rest': null,
+    'exercises': [
+      {
+        'name': 'Squats',
+        'reps': null
+      },
+      {
+        'name': 'Walkout w/ Taps',
+        'reps': null
+      },
+      {
+        'name': 'Drop Squats',
+        'reps': null
+      },
+      {
+        'name': 'Walkout w/ PushUp',
+        'reps': null
+      }
+    ]}
+
+  workout.push(warmup)
+  setTime(time - warmup.time)
+}
+
+function drawWorkout () {
+	window.console.log(workout)
+
+	let div = document.getElementById("workout")
+	let x = ""
+	for(let b of workout){
+		x = x + '<div class="block">'
+		x = x + '<h2 class="type">' + b.type + ' <span class="time"> (' + b.time + ' Min)</span></h2>'
+		if(b.rounds > 1){
+			x = x + '<span class="rounds">x' + b.rounds + ' rounds </span>'
+		}
+		if(b.rest){
+			x = x + '<span class="rest"> with ' + b.rest + ' seconds rest between</span>'
+		}
+		x = x + '<ul class="exercises">'
+		for(let e of b.exercises){
+			x = x + '<li class="exercise">' + e.name
+			if(e.reps){
+				x = x + '<span class="reps"> x' + e.reps + '</span>'
+			}
+			x = x + '</li>'
+		}
+		x = x + '</div>'
+	}
+	div.innerHTML = x
+}
+
+function generateClick () {
+	let len = document.getElementById("length").value
+	genWorkout(len)
+	drawWorkout()
+}
